a57c469cebf9fd712f1b8b7d3dcd5f5a3b5c9b92
Alan Leveque <aleveque@sio.local>
2020-11-10 09:45:48 +0100

    add files

---
+++ Contact.php
@@ -0,0 +1,7 @@
+<form method="post" action="/Contact.php?action="post">
+	<label for="email">Adresse e-mail : </label>
+	<input type="text" required id="email" name="user_e-mail" placeholder="Saisissez votre adresse e-mail">
+	<label for="Message">Message : </label>
+	<input type="text" required id="Message" name="Message" placeholder="Ecrivez ici...">
+	<input type="submit" name="Envoyer">
+</form>
