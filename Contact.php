140f09a86939a6a6d77bc7dc66bb12a4d79b02e8
Alan Lévêque <alan.leveque@sts-sio-caen.info>
2020-11-17 20:54:10 +0100

    addfiles

--- Contact.php
+++ Contact.php
@@ -1,4 +1,4 @@
-<form method="post" action="/Contact.php?action="post">
+<form method="post" action="/Contact.php?action=post">
 	<label for="email">Adresse e-mail : </label>
 	<input type="text" required id="email" name="user_e-mail" placeholder="Saisissez votre adresse e-mail">
 	<label for="Message">Message : </label>
